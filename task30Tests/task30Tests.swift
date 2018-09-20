6098aab1048f3a5ba48a1dcededa087ecccfd1b5
XORGEEK3 <emon@xorgeek.com>
2018-09-20 11:55:52 +0600

    no message

--- task30Tests/task30Tests.swift
+++ task30Tests/task30Tests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import task30
+//@testable import task30
 
 class task30Tests: XCTestCase {
     
@@ -28,7 +28,7 @@ class task30Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
